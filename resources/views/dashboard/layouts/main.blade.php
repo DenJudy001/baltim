106a1117e735253362f79db50e0d10b58709f36f
DenJudy001 <popond0001@gmail.com>
2023-04-04 17:58:53 +0700

    change dashboard ui

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -11,41 +11,112 @@
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     {{-- select2 bootstrap --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
     <!-- Custom styles for this template -->
-    <link href="/assets/css/dashboard.css" rel="stylesheet">
+    <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+
+    @stack('style')
 </head>
 
-<body>
+<body id="page-top">
 
-    @include('dashboard.layouts.partials.header')
+    <!-- Page Wrapper -->
+    <div id="wrapper">
 
-    <div class="container-fluid">
-        <div class="row">
-            @include('dashboard.layouts.partials.sidebar')
+        <!-- Sidebar -->
+        @include('dashboard.layouts.partials.sidebar')
+        <!-- End of Sidebar -->
 
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                @yield('container')
-            </main>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+
+                    <!-- Topbar Navbar -->
+                    <ul class="navbar-nav ml-auto">
+
+                        <!-- Nav Item - User Information -->
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span
+                                    class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
+                                <img class="img-profile rounded-circle"
+                                    src="{{ asset('assets/img/undraw_profile.svg') }}">
+                            </a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#"
+                                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Keluar
+                                </a>
+                                <form id="logout-form" action="/logout" method="post">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </nav>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    @yield('container')
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+
+            <!-- End of Footer -->
+
         </div>
-    </div>
+        <!-- End of Content Wrapper -->
 
-    {{-- bootstrap --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
-    </script>
-    {{-- feater icons --}}
-    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
-        integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous">
-    </script>
-    {{-- jquery --}}
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    {{-- select2 --}}
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="/assets/js/dashboard.js"></script>
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
 
-    @stack('script')
-    
+        {{-- bootstrap --}}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+        </script>
+        {{-- feater icons --}}
+        <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
+            integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous">
+        </script>
+        {{-- jquery --}}
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        {{-- select2 --}}
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+        <!-- Custom scripts for all pages-->
+        {{-- <script src="/assets/js/dashboard.js"></script> --}}
+        <script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+        @stack('script')
+
 </body>
 
 </html>
--- resources/views/dashboard/layouts/partials/sidebar.blade.php
+++ resources/views/dashboard/layouts/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+{{-- <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
     <div class="position-sticky pt-3">
         <ul class="nav flex-column">
             <li class="nav-item">
@@ -49,4 +49,81 @@
             </li>
         </ul>
     </div>
-</nav>
+</nav> --}}
+
+
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
+        <div class="sidebar-brand-text mx-3">{{ __('RM BALTIM') }}</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item {{ request()->is('account') || request()->is('account/*') ? 'active' : '' }}">
+        <a class="nav-link" href="/account">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>{{ __('Catatan Keuangan') }}</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <li class="nav-item">
+        <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+            <span>{{ __('Manajemen') }}</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item {{ request()->is('employee') || request()->is('employee/*') ? 'active' : '' }}" href="/employee"> <i class="fa fa-briefcase mr-2"></i> {{ __('Akun Karyawan') }}</a>
+                <a class="collapse-item {{ request()->is('supplier') || request()->is('supplier/*') ? 'active' : '' }}" href="/supplier"><i class="fa fa-briefcase mr-2"></i> {{ __('Pemasok') }}</a>
+                <a class="collapse-item {{ request()->is('fnb') || request()->is('fnb/*') ? 'active' : '' }}" href="/fnb"> <i class="fa fa-user mr-2"></i> {{ __('Menu') }}</a>
+            </div>
+        </div>
+    </li>
+
+     <!-- Divider -->
+     <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Pemasukan
+    </div>
+
+    <li class="nav-item {{ request()->is('pos') || request()->is('pos/*') ? 'active' : '' }}">
+        <a class="nav-link" href="/pos/create">
+            <i class="fas fa-fw fa-cogs"></i>
+            <span>{{ __('Kasir') }}</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Pengeluaran
+    </div>
+
+    <li class="nav-item {{ request()->is('purchase') || request()->is('purchase/*') ? 'active' : '' }}">
+        <a class="nav-link" href="/purchase/create">
+            <i class="fas fa-fw fa-cogs"></i>
+            <span>{{ __('Pemesanan Bahan/Barang') }}</span></a>
+    </li>
+
+    <li class="nav-item {{ request()->is('otherpurchase') || request()->is('otherpurchase/*') ? 'active' : '' }}">
+        <a class="nav-link" href="/otherpurchase/create">
+            <i class="fas fa-fw fa-cogs"></i>
+            <span>{{ __('Pembayaran Lain-lain') }}</span></a>
+    </li>
+
+    <li class="nav-item {{ request()->is('salary') || request()->is('salary/*') ? 'active' : '' }}">
+        <a class="nav-link" href="/salary/create">
+            <i class="fas fa-fw fa-cogs"></i>
+            <span>{{ __('Gaji Karyawan') }}</span></a>
+    </li>
+
+
+</ul>
